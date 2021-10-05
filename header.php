8e148ac4f33741420308e4f57dc8cb3c31c27a25
mjaumi <mjaumi2864@gmail.com>
2021-10-05 13:04:04 +0600

    image upload issue and destination header fixed

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
-<li class="nav_items"><a class="nav_link" href="#about-us-section">about us</a></li>
-<li class="nav_items"><a class="nav_link" href="#destinations-section">destinations</a></li>
-<li class="nav_items"><a class="nav_link" href="#contact-us-section">contact us</a></li>
+<li class="nav_items"><a class="nav_link" href="index.php#about-us-section">about us</a></li>
+<li class="nav_items"><a class="nav_link" href="index.php#destinations-section">destinations</a></li>
+<li class="nav_items"><a class="nav_link" href="index.php#contact-us-section">contact us</a></li>
 <li class="nav_items"><a class="nav_link" href="log-in-or-sign-up.php">log in</a></li>
 <li class="nav_items nav_item_sign_up"><div id="nav_btn_bg"><a id="nav_link_btn" href="#">sign up</a></div></li>
